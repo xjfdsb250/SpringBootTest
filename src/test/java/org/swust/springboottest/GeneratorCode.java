dfcc4a31ed00dfd2ce7dd201fd73ef5e25da60af
mashiro <xjf0218@gmail.com>
2024-12-24 14:26:34 +0800

    1

---
+++ src/test/java/org/swust/springboottest/GeneratorCode.java
@@ -0,0 +1,39 @@
+package org.swust.springboottest;
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import java.nio.file.Paths;
+import java.util.Collections;
+
+public class GeneratorCode {
+    public static void main(String[] args) {
+        FastAutoGenerator.create("jdbc:mysql://127.0.0.1:3306/sys_monitor?useUnicode=true&characterEncoding=utf-8&serverTimezone=Asia/Shanghai&useSSL=false",
+                        "root", "mysql")
+                .globalConfig(builder -> builder
+                        .author("hurenjie")
+                        .outputDir(Paths.get(System.getProperty("user.dir")) + "/src/main/java")
+                        .commentDate("yyyy-MM-dd")
+                )
+                .packageConfig(builder -> builder
+                        .parent("org.swust.springboottest")
+                        .entity("entity")
+                        .mapper("mapper")
+                        .service("service")
+                        .serviceImpl("service.impl")
+                        .xml("mapper.xml")
+                        .pathInfo(Collections.singletonMap(OutputFile.xml, System.getProperty("user.dir") + "/src/main/resources/mapper"))
+                )
+                .strategyConfig(builder -> {
+                    builder.addInclude(
+                                    "sys_user"
+                            ) // 设置需要生成的表名
+                            .entityBuilder()
+                            .enableLombok() // 启用 Lombok
+                            .enableTableFieldAnnotation() // 启用字段注解
+                            .controllerBuilder()
+                            .enableRestStyle(); // 启用 REST 风格
+                })
+                .templateEngine(new FreemarkerTemplateEngine()) // 使用 Freemarker 模板引擎
+                .execute(); // 执行生成
+    }
+}
