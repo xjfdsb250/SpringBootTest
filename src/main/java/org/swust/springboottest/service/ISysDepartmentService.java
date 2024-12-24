b8bfa76bc19371c7c80d4c0ed82c9f11f6c76113
mashiro <xjf0218@gmail.com>
2024-12-24 15:17:09 +0800

    1

---
+++ src/main/java/org/swust/springboottest/service/ISysDepartmentService.java
@@ -0,0 +1,16 @@
+package org.swust.springboottest.service;
+
+import org.swust.springboottest.entity.SysDepartment;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 部门表 服务类
+ * </p>
+ *
+ * @author hurenjie
+ * @since 2024-12-24
+ */
+public interface ISysDepartmentService extends IService<SysDepartment> {
+
+}
---
+++ src/main/java/org/swust/springboottest/service/impl/SysDepartmentServiceImpl.java
@@ -0,0 +1,20 @@
+package org.swust.springboottest.service.impl;
+
+import org.swust.springboottest.entity.SysDepartment;
+import org.swust.springboottest.mapper.SysDepartmentMapper;
+import org.swust.springboottest.service.ISysDepartmentService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 部门表 服务实现类
+ * </p>
+ *
+ * @author hurenjie
+ * @since 2024-12-24
+ */
+@Service
+public class SysDepartmentServiceImpl extends ServiceImpl<SysDepartmentMapper, SysDepartment> implements ISysDepartmentService {
+
+}
